27a121dc7933f9f1d875b082a2a0621581b3b551
Ian García <garcia.iandavid117@hotmail.com>
2025-10-21 13:31:50 -0600

    MAIN VIEW DONE

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const { app, BrowserWindow } = require('electron')
+require('electron-reload')(__dirname, {
+    electron: require(`${__dirname}/node_modules/electron`)
+})
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 450
     })
 
     win.loadFile('./views/index.html')
